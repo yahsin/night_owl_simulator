a84baed0e66b7eab8d729b4c5de39375a69b5741
yahsin <yh.horng@gmail.com>
2015-06-18 03:37:15 -0400

    audio embed

--- owlScript.js
+++ owlScript.js
@@ -1,3 +1,4 @@
+/*count Words*/
 function countWords(){
 	s = document.getElementById("inputString").value;
 	s = s.replace(/(^\s*)|(\s*$)/gi,"");
@@ -5,15 +6,24 @@ function countWords(){
 	s = s.replace(/\n /,"\n");
 	document.getElementById("wordcount").value = s.split(' ').length;
 
-	
+/*change background images*/	
 	if (s.split(' ').length <= 10) {
-		document.body.style.background = "#f3f3f3 url('night_step1.jpg') no-repeat left top ";
+		document.body.style.background = "black url('image/night_step1.jpg') no-repeat left top ";
+		document.getElementById('audiotag1').play();
+		document.getElementById('audiotag2').pause();
+		document.getElementById('audiotag3').pause();
 	} else if (s.split(' ').length <= 20){
-		document.body.style.background = "#f3f3f3 url('dawn_step2.jpg') no-repeat left top ";
+		document.body.style.background = "black url('image/dawn_step2.jpg') no-repeat left top ";
+		document.getElementById('audiotag1').pause();
+		document.getElementById('audiotag2').play();
+		document.getElementById('audiotag3').pause();
 	}else {
-		document.body.style.background = "#f3f3f3 url('morning_step1.jpg') no-repeat left top ";
+		document.body.style.background = "black url('image/morning_step1.jpg') no-repeat left top ";
+		document.getElementById('audiotag1').pause();
+		document.getElementById('audiotag2').pause();
+		document.getElementById('audiotag3').play();
 	};
-	
+
 }
 
 /* function decideBGimage() {
